f21e7d54d5ed010122559197dc41272bdfe1786c
Владимир Черников <4ev.444@gmail.com>
2015-04-27 14:27:43 +0300

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var angularApp = angular.module('angularjsFormBuilderApp', ['ui.bootstrap', 'mgcrea.ngStrap', 'ngRoute', 'DWand.nw-fileDialog'])
+//var angularApp = angular.module('angularjsFormBuilderApp', ['ui.bootstrap', '$strap.directives','ngRoute'])
+
+.config(function ($routeProvider) {
+
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        })
+        .when('/forms/create', {
+            templateUrl: 'views/create.html',
+            controller: 'CreateCtrl'
+        })
+        .when('/forms/:id/view', {
+            templateUrl: 'views/view.html',
+            controller: 'ViewCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+
+}).run(['$rootScope',  function() {}]);
+
+
