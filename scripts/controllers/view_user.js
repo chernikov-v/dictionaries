61a66405a9e7624a9b3d4d1a0e7cb2374305677c
Владимир Черников <4ev.444@gmail.com>
2015-04-29 11:04:33 +0300

    server api get post

--- scripts/controllers/view_user.js
+++ scripts/controllers/view_user.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var ViewCtrl = angularApp.controller('ViewUserCtrl', function ($scope, FormService, $routeParams, $http) {
+var ViewCtrl = angularApp.controller('ViewUserCtrl', function ($scope, FormService, $routeParams, $http, $q, $resource) {
     $scope.form = {};
 	// read form with given id
 	FormService.form($routeParams.id).then(function(form) {
@@ -8,22 +8,48 @@ var ViewCtrl = angularApp.controller('ViewUserCtrl', function ($scope, FormServi
 	});
 
   $scope.creatUser = function(){
-    $http.get('v.json').success(function(data){
-    //$http.get('users_form.json').success(function(data){
-      console.log(data);
+
+    //var api = $resource('http://localhost:3000/db',{}, {
+    var api = $resource('http://54.93.177.90/BeProductMVC/api/Forms',{}, {
+    //var api = $resource('http://84.43.208.93/BeProductMVC/api/Forms',{}, {
+      get: {
+        method: 'GET'/*,
+        /*headers: {
+          'Content-Type': 'application/json'
+        }*/
+      }
+    });
+    api.get(function(data){
+      console.log('data', data);
       $scope.form = data;
-    }).error(function(error){
-      console.log(error);
+    },function(error){
+      console.log('error', error);
     });
   };
 
   $scope.editUser = function(){
-   /* var id = prompt('Enter Existing User ID: ');
+    var id = prompt('Enter Existing User ID: ');
     console.log('ID - ', id);
-    if(id == null || id == ''){
+    if(id != null || id != ''){
+      //var api = $resource('http://84.43.208.93/BeProductMVC/api/Forms/' + id,{}, {
+      var api = $resource('http://54.93.177.90/BeProductMVC/api/Forms' + id,{}, {
+        get: {
+          method: 'GET'
+        }
+      });
 
-    }*/
+      api.get(function(data){
+        console.log('data - ', data);
+        $scope.form = data;
+      },function(error){
+        console.log('error - ', error);
+      });
+    }
+
   };
 
+  $scope.sendForm = function(){
+    alert('send');
+  }
 
 });
