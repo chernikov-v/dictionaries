896a4e5b8c4b0f2c3c21bfae2356b7df367b17a5
Владимир Черников <4ev.444@gmail.com>
2015-05-18 15:52:54 +0300

    listView edit, add and filter

--- scripts/directives/list-directive.js
+++ scripts/directives/list-directive.js
@@ -10,42 +10,60 @@ angularApp.directive('list', function ($http, $location, $timeout, Api) {
     //},
     link: function ($scope, $element, $attrs, $controller) {
 
+      $http.get('static-data/list.json').success(function (response) {
 
-      $scope.datasource = new kendo.data.DataSource({
-        transport: {
-          read: "static-data/list.json"
-        },
-        pageSize: 4
-      });
-
-      $scope.pager = $element.find('#pager').kendoPager({
-        dataSource: $scope.datasource,
-        pageSize: 4,
-        schema: {
-          model: {
-            id: "ProductID",
-            fields: {
-              ProductID: { editable: false, nullable: true },
-              ProductName: "ProductName",
-              UnitPrice: { type: "number" },
-              Discontinued: { type: "boolean" },
-              UnitsInStock: { type: "number" }
+        $scope.datasource = new kendo.data.DataSource({
+          data: response,
+          /*transport: {
+            read: "static-data/list.json"
+          },*/
+          pageSize: 4,
+          schema: {
+            model: {
+              id: "ProductID",
+              fields: {
+                ProductID: {editable: false, nullable: true},
+                ProductName: {type: "string"},
+                UnitPrice: {type: "number"},
+                Discontinued: {type: "boolean"},
+                UnitsInStock: {type: "number"}
+              }
             }
           }
-        }
-      }).data("kendoPager");
+        });
 
-      $scope.listView = $element.find('#listView').kendoListView({
-        dataSource: $scope.datasource,
-        template: kendo.template($("#template").html()),
-        editTemplate: kendo.template($("#editTemplate").html())
-      }).data("kendoListView");
+        $scope.pager = $element.find('#pager').kendoPager({
+          dataSource: $scope.datasource
+        }).data("kendoPager");
 
-      $(".k-add-button").click(function(e) {
-        $scope.listView.add();
-        e.preventDefault();
+        $scope.listView = $element.find('#listView').kendoListView({
+          dataSource: $scope.datasource,
+          template: kendo.template($("#template").html()),
+          editTemplate: kendo.template($("#editTemplate").html())
+        }).data("kendoListView");
+
+        $(".k-add-button").click(function (e) {
+          $scope.listView.add();
+          e.preventDefault();
+        });
+
+        $('#find').click(function(e){
+          var filterVal = $('#filter').val();
+          console.log(filterVal.length);
+          if(filterVal){
+            console.log($scope.listView);
+            $scope.listView.dataSource.filter( { field: "ProductName", operator: "contains", value: filterVal});
+          }
+          e.preventDefault();
+        });
+
+        $('#clearFind').click(function(e){
+          var filterVal = $('#filter').val("");
+            $scope.listView.dataSource.filter({});
+          e.preventDefault();
+        });
+
       });
-
     }
   }
 });
