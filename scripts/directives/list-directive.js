4d50fc7769a79bd807f7955caa9406e211028232
Владимир Черников <4ev.444@gmail.com>
2015-05-19 16:17:23 +0300

    ListView server get generate list item template + edit template generate search fields

--- scripts/directives/list-directive.js
+++ scripts/directives/list-directive.js
@@ -1,65 +1,142 @@
 'use strict';
 
 
-angularApp.directive('list', function ($http, $location, $timeout, Api) {
+angularApp.directive('list', function ($http, Api, listTemplateFactory) {
 
   return {
     restrict: 'EA',
-    //scope: {
-    //  onEdit: '='
-    //},
     link: function ($scope, $element, $attrs, $controller) {
 
-      $http.get('static-data/list.json').success(function (response) {
+      //$http.get('static-data/list.json').success(function (response) {
+      $http.get(Api.urls.listviewschema).success(function (response) {
 
+        $scope.schema = response.schema;
+        $scope.columns = response.columns;
+
+        /*===========================================================================================*/
+        /*DATA SOURCE*/
+        /*===========================================================================================*/
         $scope.datasource = new kendo.data.DataSource({
-          data: response,
-          /*transport: {
-            read: "static-data/list.json"
-          },*/
-          pageSize: 4,
+          transport: {
+            read: function(options){
+              console.log(options);
+              $.ajax({
+                url: Api.urls.listview,
+                data: {
+                  models: kendo.stringify(options.data)
+                },success: function(result) {
+                  options.success(result.data);
+                }
+              })
+            },
+            create: function(options) {
+              console.log('create');
+              $.ajax({
+                method: "POST",
+                url: "http://demos.telerik.com/kendo-ui/service/products/create",
+                dataType: "json",
+                data: {
+                  models: kendo.stringify(options.data)
+                },
+                success: function(result) {
+                  options.success(result);
+                }
+              });
+            },
+            update: function(options) {
+              console.log('update');
+              $.ajax({
+                method: "POST",
+                url: "http://demos.telerik.com/kendo-ui/service/products/create",
+                data: {
+                  models: kendo.stringify(options.data)
+                },
+                success: function(result) {
+                  options.success(result);
+                }
+              });
+            },
+            destroy: function(options) {
+              console.log('destroy');
+              $.ajax({
+                url: "http://demos.telerik.com/kendo-ui/service/products/create/id",
+                data: {
+                  models: kendo.stringify(options.data.models)
+                },
+                success: function(result) {
+                  options.success(result);
+                }
+              });
+            }
+          },
           schema: {
             model: {
-              id: "ProductID",
-              fields: {
-                ProductID: {editable: false, nullable: true},
-                ProductName: {type: "string"},
-                UnitPrice: {type: "number"},
-                Discontinued: {type: "boolean"},
-                UnitsInStock: {type: "number"}
-              }
+              id: "id",
+              fields:  $scope.schema
             }
-          }
+          },
+          pageSize: 4
         });
 
+        /*===========================================================================================*/
+        /*PAGER*/
+        /*===========================================================================================*/
         $scope.pager = $element.find('#pager').kendoPager({
           dataSource: $scope.datasource
         }).data("kendoPager");
 
+        /*===========================================================================================*/
+        /*LIST*/
+        /*===========================================================================================*/
+
         $scope.listView = $element.find('#listView').kendoListView({
           dataSource: $scope.datasource,
-          template: kendo.template($("#template").html()),
-          editTemplate: kendo.template($("#editTemplate").html())
+          template: listTemplateFactory.template($scope.columns, $scope.schema),
+          editTemplate: listTemplateFactory.editTemplate($scope.columns, $scope.schema)
         }).data("kendoListView");
 
+        /*===========================================================================================*/
+        /*Additional interface elements*/
+        /*===========================================================================================*/
+        var filter = $('#filter').append('<table></table>');
+        var table = filter.find("table");
+        for(var i = 0; i < $scope.columns.length; i++){
+          if(typeof $scope.columns[i].filterable != 'undefined'){
+            var filterId = "filter" + $scope.columns[i].field;
+            table.append('<tr><td style="width: 200px"><lable>Filter by '+ $scope.columns[i].title +'</lable></td><td><input id="'+ filterId +'"/></td></tr>');
+          }
+        }
+
+        /*Find button*/
+        $('#find').click(function(e){
+          var filterParams = [];
+          for(var i = 0; i < $scope.columns.length; i++){
+            if(typeof $scope.columns[i].filterable != 'undefined') {
+              var filterVal = $("#filter" + $scope.columns[i].field).val();
+              if (filterVal) {
+                filterParams.push({ field: $scope.columns[i].field, operator: "contains", value: filterVal})
+              }
+            }
+          }
+          console.log(filterParams);
+          $scope.listView.dataSource.filter( filterParams );
+          e.preventDefault();
+        });
+
+        /*clear find button*/
+        $('#clearFind').click(function(e){
+          for(var i = 0; i < $scope.columns.length; i++) {
+            if (typeof $scope.columns[i].filterable != 'undefined') {
+              $("#filter" + $scope.columns[i].field).val("");
+            }
+          }
+          $scope.listView.dataSource.filter({});
+          e.preventDefault();
+        });
+
+          /*add button*/
         $(".k-add-button").click(function (e) {
           $scope.listView.add();
-          e.preventDefault();
-        });
-
-        $('#find').click(function(e){
-          var filterVal = $('#filter').val();
-          console.log(filterVal.length);
-          if(filterVal){
-            console.log($scope.listView);
-            $scope.listView.dataSource.filter( { field: "ProductName", operator: "contains", value: filterVal});
-          }
-          e.preventDefault();
-        });
-
-        $('#clearFind').click(function(e){
-          var filterVal = $('#filter').val("");
-            $scope.listView.dataSource.filter({});
           e.preventDefault();
         });
 
--- scripts/services/api.js
+++ scripts/services/api.js
@@ -1,36 +1,36 @@
 angularApp.factory('Api', function ($resource, $http, $q) {
 
-/*
-*
-*
-*
- GET http://mvc.gloria-jeans-portal.com/api/forms/get получить форму с новым юзером
- GET http://mvc.gloria-jeans-portal.com/api/forms/get/a93bd039-4328-42ce-7456-8b8f8d26fcf8 получить юзера по ID
- POST http://mvc.gloria-jeans-portal.com/api/forms/upsert добавляем нового или изменяем текущего формат отправляемых данных тот же
- GET http://mvc.gloria-jeans-portal.com/api/forms/list получаем список данных для gridview
- GET http://mvc.gloria-jeans-portal.com/api/forms/schema получаем схему для gridview
- GET http://mvc.gloria-jeans-portal.com/api/forms/editadd добавляем или изменяем элемент
- GET http://mvc.gloria-jeans-portal.com/api/forms/delete/a140d039-e4ac-d1cb-3726-bc91e04df936
-*
-*
-*
-*
-* */
+  /*
+   *
+   *
+   *
+   GET http://mvc.gloria-jeans-portal.com/api/forms/get получить форму с новым юзером
+   GET http://mvc.gloria-jeans-portal.com/api/forms/get/a93bd039-4328-42ce-7456-8b8f8d26fcf8 получить юзера по ID
+   POST http://mvc.gloria-jeans-portal.com/api/forms/upsert добавляем нового или изменяем текущего формат отправляемых данных тот же
+   GET http://mvc.gloria-jeans-portal.com/api/forms/list получаем список данных для gridview
+   GET http://mvc.gloria-jeans-portal.com/api/forms/schema получаем схему для gridview
+   GET http://mvc.gloria-jeans-portal.com/api/forms/editadd добавляем или изменяем элемент
+   GET http://mvc.gloria-jeans-portal.com/api/forms/delete/a140d039-e4ac-d1cb-3726-bc91e04df936
+   *
+   *
+   *
+   *
+   * */
 
-var mainUrl = 'http://mvc.gloria-jeans-portal.com/api/';
+  var mainUrl = 'http://mvc.gloria-jeans-portal.com/api/';
 
-  var formUrl =  mainUrl + "forms/";
+  var formUrl = mainUrl + "forms/";
   var gridUrl = mainUrl + "User/";
 
 
-var apiURLs = {
-    get : formUrl + "get",
-    getById: function(id){
+  var apiURLs = {
+    get: formUrl + "get",
+    getById: function (id) {
       var url;
       if (typeof id != 'undefined' && id != null && id != '') {
-        url =  formUrl + 'get/' + id;
+        url = formUrl + 'get/' + id;
       } else {
-        url =  formUrl + 'get'
+        url = formUrl + 'get'
       }
       return url;
     },
@@ -38,26 +38,21 @@ var apiURLs = {
     gridConfig: gridUrl + 'grid',
     gridList: gridUrl + 'list',
     gridSchema: gridUrl + 'schema',
-    editadd: gridUrl+ "editadd",
-    deleteItem: function(id){
-    return gridUrl + 'delete/' + id;
-  }
+    editadd: gridUrl + "editadd",
+    deleteItem: function (id) {
+      return gridUrl + 'delete/' + id;
+    },
+    listviewschema: mainUrl + "user/listviewschema",
+    listview: mainUrl + "/user/listview"
 
-};
+  };
   //var apiUrl = 'http://54.93.177.90/BeProductMVC/api/Forms';
 
   //var apiUrl = 'http://84.43.208.93/BeProductMVC/api/Forms';
   //var apiUrl = 'http://localhost:3000/db';
 
 
-
-
-
-
-
-
- var get = function(_data, id) {
-
+  var get = function (_data, id) {
 
 
     var api = $resource(apiURLs.getById(id), {}, {
@@ -68,10 +63,10 @@ var apiURLs = {
 
     api.get(function (response) {
       console.log('data - ', response);
-      if(typeof response.data_id == 'undefined'){
+      if (typeof response.data_id == 'undefined') {
         alert('No data received! Incorrect ID!')
-      }else{
-        for(var i in response){
+      } else {
+        for (var i in response) {
           _data[i] = response[i];
         }
       }
@@ -79,11 +74,10 @@ var apiURLs = {
     });
   };
 
-  var jqPost = function(_data, id) {
+  var jqPost = function (_data, id) {
     return $.post(
       apiURLs.post,
-      { data: angular.toJson(_data) }
-
+      {data: angular.toJson(_data)}
     )
   };
 
