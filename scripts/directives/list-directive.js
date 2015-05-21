1c4653f2c8ad0946fea3be9e5bf4a4673e2e626c
Владимир Черников <4ev.444@gmail.com>
2015-05-21 17:17:19 +0300

    List server paging list server filtering

--- scripts/directives/list-directive.js
+++ scripts/directives/list-directive.js
@@ -16,15 +16,38 @@ angularApp.directive('list', function ($http, Api, listTemplateFactory) {
         /*===========================================================================================*/
         /*DATA SOURCE*/
         /*===========================================================================================*/
+        var filterParser = function (obj) {
+          var tmp = [];
+          if (typeof obj.filters == 'undefined') {
+            tmp.push(obj)
+          } else {
+            for (var j = 0; j < obj.filters.length; j++) {
+              tmp = tmp.concat(filterParser(obj.filters[j]));
+            }
+          }
+          return tmp;
+        };
+
         $scope.datasource = new kendo.data.DataSource({
           transport: {
             read: function(options){
               console.log(options);
+
+              var data = options.data;
+
+              if (typeof data.filter != 'undefined') {
+                console.log('result ', filterParser(data.filter));
+                data.filter = filterParser(options.data.filter);
+              }
+
               $.ajax({
-                url: Api.urls.listview,
+                method:"POST",
+                //url: Api.urls.listview,
+                url: Api.urls.gridList,
                 data: {
-                  models: kendo.stringify(options.data)
+                  data: kendo.stringify(data)
                 },success: function(result) {
+                  result.data.total = result.total;
                   options.success(result.data);
                 }
               })
@@ -32,57 +55,62 @@ angularApp.directive('list', function ($http, Api, listTemplateFactory) {
             create: function(options) {
               console.log('create');
               $.ajax({
-                method: "POST",
-                url: "http://demos.telerik.com/kendo-ui/service/products/create",
-                dataType: "json",
+                method: 'POST',
+                url: Api.urls.editadd,
                 data: {
-                  models: kendo.stringify(options.data)
+                  data: kendo.stringify(options.data)
                 },
-                success: function(result) {
-                  options.success(result);
+                success: function (result) {
+                  options.data.id = result.id;
+                  options.success(options.data);
                 }
               });
             },
             update: function(options) {
               console.log('update');
               $.ajax({
-                method: "POST",
-                url: "http://demos.telerik.com/kendo-ui/service/products/create",
+                method: 'POST',
+                url: Api.urls.editadd,
                 data: {
-                  models: kendo.stringify(options.data)
+                  data: kendo.stringify(options.data)
                 },
-                success: function(result) {
-                  options.success(result);
+                success: function (result) {
+                  options.data.id = result.id;
+                  options.success(options.data);
                 }
               });
             },
             destroy: function(options) {
               console.log('destroy');
               $.ajax({
-                url: "http://demos.telerik.com/kendo-ui/service/products/create/id",
-                data: {
-                  models: kendo.stringify(options.data.models)
-                },
-                success: function(result) {
+                url: Api.urls.deleteItem(options.data._id),
+                success: function (result) {
                   options.success(result);
                 }
               });
             }
           },
           schema: {
+            total: "total",
             model: {
               id: "id",
               fields:  $scope.schema
             }
           },
-          pageSize: 4
+          pageSize: 4,
+          serverPaging: true,
+          serverFiltering: true,
+          serverSorting: true
         });
 
         /*===========================================================================================*/
         /*PAGER*/
         /*===========================================================================================*/
         $scope.pager = $element.find('#pager').kendoPager({
-          dataSource: $scope.datasource
+          dataSource: $scope.datasource,
+          pageSize: 4,
+          previousNext: true,
+          numeric: true
         }).data("kendoPager");
 
         /*===========================================================================================*/
